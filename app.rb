1b7e88f4885c7a64aed292e64b71168e4d2af9d5
Paul Martin <communty.hivemind@gmail.com>
2019-03-19 16:15:42 +0000

    refactor to store names in (global!) Player objects rather then session keys

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/player.rb'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -8,19 +9,23 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:name1], session[:name2] = params[:name1], params[:name2]
+    $player_1 = Player.new(params[:name1])
+    $player_2 = Player.new(params[:name2])
+    # session[:name1], session[:name2] = params[:name1], params[:name2]
     session[:hp_p1], session[:hp_p2] = 60, 60
     redirect '/play'
   end
 
   get '/play' do
-    @name1, @name2 = session[:name1], session[:name2]
+    # @name1, @name2 = session[:name1], session[:name2]
+    @name1, @name2 = $player_1.name, $player_2.name
     @hp_p1, @hp_p2 = session[:hp_p1], session[:hp_p2]
     erb :play
   end
 
   get '/attack' do
-    @name1, @name2 = session[:name1], session[:name2]
+    # @name1, @name2 = session[:name1], session[:name2]
+    @name1, @name2 = $player_1.name, $player_2.name
     erb :attack
   end
 end
